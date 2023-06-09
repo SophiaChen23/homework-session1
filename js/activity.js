f939fd798fcfdfe311376ddf26b9dfd860654297
sophia <zchen9523@gmail.com>
2023-06-08 19:27:46 -0500

    update w5d3

--- js/activity.js
+++ js/activity.js
@@ -3,10 +3,27 @@
 $(document).ready(function() {
     // Add click event handler to selectable cells
     $('.selectable').click(function() {
+        var content = $(this).text();
         // Check if the cell is available (not 'Not Available') and not a heading cell
-        if (!$(this).hasClass('heading') && $(this).text() !== 'Not Available') {
+        if (content !== 'Not Available') {
             // Toggle the selected class
+
             $(this).toggleClass('selected');
+
+
+            if ($(this).hasClass('selected')) {
+                $('#displaySelected').css("visibility", "visible");
+                $('#displaySelected').css("margin-top", "2em");
+                $('#result').append("<p>" + content + "</p>" );
+
+            } else {
+                $('#result p:contains('+content+')' +')').remove();
+
+                if ( $('#result').has('p').length == false) {
+                    $('#displaySelected').css("visibility", "hidden");
+                    $('#displaySelected').css("margin-top", "0");
+                }
+            }
         }
     });
 });
