0ddd915f3ec1ed1e2719eeb12596e48039b78913
sophia <zchen9523@gmail.com>
2023-06-16 01:34:31 -0500

    make some commit

--- js/activity.js
+++ js/activity.js
@@ -1,29 +1,55 @@
-
-
 $(document).ready(function() {
     // Add click event handler to selectable cells
     $('.selectable').click(function() {
-        var content = $(this).text();
+        var columnIndex = $(this).index();
+        var rows = $('table tbody tr');
+
         // Check if the cell is available (not 'Not Available') and not a heading cell
-        if (content !== 'Not Available') {
+        if ($(this).text() !== 'Not Available') {
             // Toggle the selected class
-
             $(this).toggleClass('selected');
-
-
-            if ($(this).hasClass('selected')) {
-                $('#displaySelected').css("visibility", "visible");
-                $('#displaySelected').css("margin-top", "2em");
-                $('#result').append("<p>" + content + "</p>" );
-
-            } else {
-                $('#result p:contains('+content+')' +')').remove();
-
-                if ( $('#result').has('p').length == false) {
-                    $('#displaySelected').css("visibility", "hidden");
-                    $('#displaySelected').css("margin-top", "0");
-                }
-            }
+            updateSelectedCells(columnIndex, rows);
+            // updateSendInfoBox(columnIndex);
         }
     });
+
+    // Function to retrieve and display selected cells
+    function updateSelectedCells(columnIndex, rows) {
+        var selectedCells = $('.selectable.selected');
+        var resultDiv = $('#result');
+        resultDiv.empty();
+
+        if (selectedCells.length > 0) {
+            var heading = $('<h4>My selected activities to inquire:</h4>');
+            resultDiv.append(heading);
+
+            selectedCells.each(function() {
+                var content = $(this).text();
+                var columnIndex = $(this).index();
+                var columnHeader = $('table thead th').eq(columnIndex).text();
+                var paragraph = $('<p>').text(content + ' at ' + columnHeader);
+                resultDiv.append(paragraph);
+            });
+
+            $('#displaySelected').css("visibility", "visible");
+            $('#displaySelected').css("margin-top", "2em");
+        } else {
+            $('#displaySelected').css("visibility", "hidden");
+            $('#displaySelected').css("margin-top", "0");
+        }
+    }
+
+
+
+    // Function to retrieve and display selected cell and corresponding column name from thead in sendinfo box
+    // Function to retrieve and display selected cell and corresponding column name from thead in sendinfo box
+    // Function to retrieve and display selected cell and corresponding column name from thead in sendinfo box
+    // Function to retrieve and display selected cell and corresponding column name from thead in sendinfo box
+
+
+
+
+
+
+
 });
